91bc1b5ffa6dd6024ca9bf711f5a50bea1c59e18
yaling888 <73897884+yaling888@users.noreply.github.com>
2021-03-24 04:18:30 +0800

    Update config.pb.go

--- rules/router/config.pb.go
+++ rules/router/config.pb.go
@@ -7,8 +7,6 @@
 package router
 
 import (
-        //proto "github.com/golang/protobuf/proto"
-        proto "google.golang.org/protobuf/proto"
         protoreflect "google.golang.org/protobuf/reflect/protoreflect"
         protoimpl "google.golang.org/protobuf/runtime/protoimpl"
         reflect "reflect"
@@ -21,10 +19,6 @@ const (
         // Verify that runtime/protoimpl is sufficiently up-to-date.
         _ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
-
-// This is a compile-time assertion that a sufficiently up-to-date version
-// of the legacy proto package is being used.
-const _ = proto.ProtoPackageIsVersion4
 
 // Type of domain value.
 type Domain_Type int32
