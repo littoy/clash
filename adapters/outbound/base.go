8d87c7e19b5024d8e8573873df91eee1f557d446
fishg <1423545+fishg@users.noreply.github.com>
2021-06-10 20:21:13 +0800

    补充日志打印

--- adapters/outbound/base.go
+++ adapters/outbound/base.go
@@ -194,7 +194,8 @@ func (p *Proxy) DialContext(ctx context.Context, metadata *C.Metadata) (C.Conn, 
 	if err != nil {
 		p.SetFailCount(p.FailCount() + 1)
 		if p.FailCount() >= p.MaxFail() {
-			log.Errorln("proxy dead of error: %s %s %s", p.Name(), time.Now().Format("2006-01-02 15:04:05"), err.Error())
+			// log.Errorln("proxy dead of error: %s %s %s", p.Name(), time.Now().Format("2006-01-02 15:04:05"), err.Error())
+			log.Errorln("proxy dead of error: %s %s", p.Name(), err.Error())
 			p.alive.Store(false)
 			if p.ForbidDuration() > 0 && p.DownFrom() == 0 {
 				p.SetDownFrom(time.Now().Unix())
@@ -271,7 +272,8 @@ func (p *Proxy) URLTest(ctx context.Context, url string) (t uint16, l uint16, er
 		if err != nil || t >= uint16(p.Timeout()) || l >= uint16(p.MaxLoss()) {
 			p.SetFailCount(p.FailCount() + 1)
 			if p.FailCount() >= p.MaxFail() {
-				log.Errorln("proxy dead of error: %s %s duration: %d loss: %d", p.Name(), time.Now().Format("2006-01-02 15:04:05"), t, l)
+				// log.Errorln("proxy dead of error: %s %s duration: %d loss: %d", p.Name(), time.Now().Format("2006-01-02 15:04:05"), t, l)
+				log.Errorln("proxy dead of error: %s duration: %d loss: %d e: %s", p.Name(), t, l, err.Error())
 				p.alive.Store(false)
 				if p.ForbidDuration() > 0 && p.DownFrom() == 0 {
 					p.SetDownFrom(time.Now().Unix())
